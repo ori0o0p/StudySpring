81394f91576eabf5a3f89cd056c151ef00aa4444
ori0o0p <openwund8517@gmail.com>
2023-07-25 22:40:32 +0900

    edit user

--- src/main/java/com/example/studyspring/user/entity/User.java
+++ src/main/java/com/example/studyspring/user/entity/User.java
@@ -2,28 +2,26 @@ package com.example.studyspring.user.entity;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
-public class user {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
-
+    @Column
     private String email;
-
+    @Column
     private String accountId;
-
+    @Column
     private String password;
+
 
 }
