75b9411627319bc0308d2f0ef4ed8211679db98a
ori0o0p <openwund8517@gmail.com>
2023-07-26 23:04:00 +0900

    add login service

---
+++ src/main/java/com/example/studyspring/domain/user/service/LoginService.java
@@ -0,0 +1,14 @@
+package com.example.studyspring.domain.user.service;
+
+import com.example.studyspring.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+}
