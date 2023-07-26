fbed4347ea381f0ec5ddb749c0f9d56c50a59ac9
ori0o0p <openwund8517@gmail.com>
2023-07-26 19:50:21 +0900

    edit login request

--- src/main/java/com/example/studyspring/domain/user/dto/request/LoginRequest.java
+++ src/main/java/com/example/studyspring/domain/user/dto/request/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.example.studyspring.domain.user.dto.request;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class LoginRequest {
 
     private String accountId;
