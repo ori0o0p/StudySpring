9b7e4cec25fc06d9f33de9cd08a44ab093c045ae
ori0o0p <openwund8517@gmail.com>
2023-07-26 19:54:49 +0900

    add user controller

---
+++ src/main/java/com/example/studyspring/domain/user/dto/UserController.java
@@ -0,0 +1,22 @@
+package com.example.studyspring.domain.user.dto;
+
+import com.example.studyspring.domain.user.dto.request.SignupRequest;
+import com.example.studyspring.domain.user.service.SignupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+    private final SignupService signupService;
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody SignupRequest request) {
+        signupService.execute(request);
+    }
+
+}
