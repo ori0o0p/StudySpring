7debd6c5641950ae17b3a3053573e36e8e809ffc
ori0o0p <openwund8517@gmail.com>
2023-07-27 12:36:48 +0900

    edit user service

--- src/main/java/com/example/studyspring/global/security/JwtService.java
+++ src/main/java/com/example/studyspring/global/security/JwtService.java
@@ -4,12 +4,19 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Date;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtService {
 
@@ -29,5 +36,28 @@ public class JwtService {
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
+
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e){
+            log.error("토큰 오류");
+        }
+        return false;
+    }
+
+    public Claims parseClaims(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claim = parseClaims(token);
+
+        UserDetails details = new User(claim.getSubject(), "", Collections.emptyList());
+        return new UsernamePasswordAuthenticationToken(details, "", details.getAuthorities());
+    }
+
 
 }
