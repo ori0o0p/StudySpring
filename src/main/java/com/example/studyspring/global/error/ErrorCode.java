de5ede4532f28b265007b82ae755d03df41c18bd
ori0o0p <openwund8517@gmail.com>
2023-07-30 01:06:10 +0900

    add error code

--- src/main/java/com/example/studyspring/global/error/ErrorCode.java
+++ src/main/java/com/example/studyspring/global/error/ErrorCode.java
@@ -9,6 +9,8 @@ public enum ErrorCode {
     ALREADY_EXIST(400, "user already exist"),
     PASSWORD_NOT_MATCHES(400, "incorrect password"),
 
+    INVALID_TOKEN(401, "유효하지 않은 토큰"),
+
     USER_NOT_FOUND(404, "user not found");
 
     private final int httpStatus;
