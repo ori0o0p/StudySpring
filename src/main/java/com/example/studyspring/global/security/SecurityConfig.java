41d66ab34e573695caeaa0c50b7ac0caf21af56f
ori0o0p <openwund8517@gmail.com>
2023-07-29 12:06:43 +0900

    edit security config

--- src/main/java/com/example/studyspring/global/security/SecurityConfig.java
+++ src/main/java/com/example/studyspring/global/security/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.studyspring.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +14,23 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/**").permitAll()
+                .anyRequest().authenticated();
+        return httpSecurity.build();
     }
 
 }
