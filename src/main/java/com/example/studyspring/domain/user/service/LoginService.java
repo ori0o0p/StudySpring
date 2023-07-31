e08a7f16a5b3f4a9918c562cedebe669d1553fdf
ori0o0p <openwund8517@gmail.com>
2023-08-01 00:19:27 +0900

    add login

--- src/main/java/com/example/studyspring/domain/user/service/LoginService.java
+++ src/main/java/com/example/studyspring/domain/user/service/LoginService.java
@@ -1,6 +1,12 @@
 package com.example.studyspring.domain.user.service;
 
+import com.example.studyspring.domain.user.Exception.PasswordNotMatchedException;
+import com.example.studyspring.domain.user.Exception.UserNotFoundException;
+import com.example.studyspring.domain.user.dto.request.LoginRequest;
+import com.example.studyspring.domain.user.dto.response.LoginResponse;
+import com.example.studyspring.domain.user.entity.User;
 import com.example.studyspring.domain.user.repository.UserRepository;
+import com.example.studyspring.global.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,5 +16,24 @@ import org.springframework.stereotype.Service;
 public class LoginService {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
+    private final JwtProvider jwtProvider;
+
+    public LoginResponse execute(LoginRequest request) {
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (!encoder.matches(request.getPassword(), user.getPassword())) {
+            throw PasswordNotMatchedException.EXCEPTION;
+        }
+
+        String accessToken = jwtProvider.createAccessToken(request.getAccountId());
+        String refreshToken = jwtProvider.createRefreshToken(request.getAccountId());
+
+        return LoginResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
 
 }
