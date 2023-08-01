a409e69ca31ba81e82d4a044945f22545fe34930
ori0o0p <openwund8517@gmail.com>
2023-08-01 10:10:44 +0900

    add login

--- src/main/java/com/example/studyspring/domain/user/dto/UserController.java
+++ src/main/java/com/example/studyspring/domain/user/dto/UserController.java
@@ -1,22 +1,28 @@
 package com.example.studyspring.domain.user.dto;
 
+import com.example.studyspring.domain.user.dto.request.LoginRequest;
 import com.example.studyspring.domain.user.dto.request.SignupRequest;
+import com.example.studyspring.domain.user.dto.response.LoginResponse;
+import com.example.studyspring.domain.user.service.LoginService;
 import com.example.studyspring.domain.user.service.SignupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class UserController {
     private final SignupService signupService;
+    private final LoginService loginService;
 
     @PostMapping("/signup")
     public void signup(@RequestBody SignupRequest request) {
         signupService.execute(request);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return loginService.execute(request);
     }
 
 }
